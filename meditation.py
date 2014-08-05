eaa812dcd5518f9f944eb1566a9b3d3a6875c91d
kbrugh <keenan@NussPSGs-MacBook-Pro-3.local>
2014-08-05 13:48:07 -0600

    Meditation.py
    This program allows you to program your own mind. It works by mixing in
    user-defined meditations with randomly generated characters. The
    concept is that your brain will recognize the pattern. Many, many times.

---
+++ meditation.py
@@ -0,0 +1,18 @@
+# Meditation
+#
+# This program allows you to program your own mind. It works by
+# mixing in userdefined meditations with randomly generated characters. The
+# concept is that your brain will recognize the pattern. Many, many times.
+ 
+import os, random, time
+ 
+def lucid(meditation):
+    for i in xrange(0, 30000):
+        time.sleep(.0714)
+        print os.urandom(256), meditation, os.urandom(random.randint(0, 256)).replace("?", "x")
+
+ 
+if __name__ == '__main__':
+    meditation = raw_input("Enter your meditation: ")
+    lucid(meditation)
+	

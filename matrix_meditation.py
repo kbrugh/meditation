2a279bf3ed0cd1ea16282e7d915f07af7cacf787
kbrugh <keenan@NussPSGs-MacBook-Pro-3.local>
2014-08-26 15:31:29 -0600

    Matrix_meditation
    This version of matrix_meditation.py lets you enter your meditation,
    select the print-rate in Hz for brainwave entrainment, and the time of
    the session.

---
+++ matrix_meditation.py
@@ -0,0 +1,42 @@
+# Matrix Modelling, Meditation, & Motivation
+#	Prototype August 2014
+#
+# This program allows you to program your own mind. It works by
+# mixing in userdefined meditations with randomly generated characters. The
+# concept is that your brain will recognize the pattern. Many, many times.
+#
+# The print rate coding allows a mechanism to set a specifc target brainwave frequency.
+
+ 
+import os, random, time
+ 
+def lucid(meditation):
+    for i in xrange(0, int(timemax)):
+        time.sleep(delay)
+        print os.urandom(256), meditation, os.urandom(random.randint(0, 256))
+
+ 
+if __name__ == '__main__':
+    meditation = raw_input("Enter meditation: ")
+    Hz = float(raw_input("Enter brainwave frequency, in Hz: "))
+    timer = int(raw_input("Enter time, in minutes: "))
+    delay = 1 / Hz
+    timemax = ((timer * 60) / (1 / Hz))
+    lucid(meditation)
+	
+# Next To Do:
+#
+# Select new data into the matrix. Thinking about a flow (time.date?) 
+# Add auditory tones
+# test and ensure bitrate and frequency --- close enough! - at 10 Hz, the avg error is .72ms or .00072 seconds 
+# Wanting to add a range shifting function that can move from an initial Hz to a target resulting Hz.
+# Make it hosted on the internet
+# Incentives?
+# Stimulation and relaxation - arousal levels in balance, specific for the context
+#
+# Sex, hunger(survive), need to belong
+
+
+# Use cases: eating habits,
+
+# Language considertations: use simple meditations with nouns, verbs, and adjectives

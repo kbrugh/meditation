3547be8547148f10e6c1af772e104e4f6eab7fc7
kbrugh <kbrugh@gmail.com>
2015-08-05 11:20:00 -0600

    Update matrix_meditation.py

--- matrix_meditation.py
+++ matrix_meditation.py
@@ -1,13 +1,3 @@
-# Matrix Modelling, Meditation, & Motivation
-#	Prototype August 2014
-#
-# This program allows you to program your own mind. It works by
-# mixing in userdefined meditations with randomly generated characters. The
-# concept is that your brain will recognize the pattern. Many, many times.
-#
-# The print rate coding allows a mechanism to set a specifc target brainwave frequency.
-
- 
 import os, random, time
  
 def lucid(meditation):
@@ -23,14 +13,3 @@ if __name__ == '__main__':
     delay = 1 / Hz
     timemax = ((timer * 60) / (1 / Hz))
     lucid(meditation)
-	
-# Next To Do:
-'''
--Select new data into the matrix. Thinking about a flow (time.date?) 
--Add auditory tones
--test and ensure bitrate and frequency --- close enough! - at 10 Hz, the avg error is .72ms or .00072 seconds 
--Wanting to add a range shifting function that can move from an initial Hz to a target resulting Hz.
--Make it hosted on the internet
--Incentives?
--Stimulation and relaxation - arousal levels in balance, specific for the context
--Language considerations: use simple meditations with nouns, verbs, and adjectives'''
